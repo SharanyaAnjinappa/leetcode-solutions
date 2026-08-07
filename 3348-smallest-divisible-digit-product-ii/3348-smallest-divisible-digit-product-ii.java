d535e4db7604ced935c5d187a48676eae3facde1
Sharanya.A <eng23cs0183@dsu.edu.in>
2026-08-07 22:10:40 +0530

    Time: 22 ms (57.14%), Space: 50.2 MB (51.43%) - LeetHub

--- 3348-smallest-divisible-digit-product-ii/3348-smallest-divisible-digit-product-ii.java
+++ 3348-smallest-divisible-digit-product-ii/3348-smallest-divisible-digit-product-ii.java
@@ -8,94 +8,62 @@ class Solution {
         }
         return a;
     }
-
     public String smallestNumber(String num, long t) {
-
         long temp = t;
-
         for (int i = 2; i <= 9; i++) {
             while (temp % i == 0) {
                 temp /= i;
             }
         }
-
         if (temp > 1) {
             return "-1";
         }
-
         int n = num.length();
-
         long[] rem = new long[n + 1];
         rem[0] = t;
-
         int pos = n - 1;
-
         char[] arr = num.toCharArray();
-
         for (int i = 0; i < n; i++) {
-
             if (arr[i] == '0') {
                 pos = i;
                 break;
             }
-
             rem[i + 1] = rem[i] / gcd(rem[i], arr[i] - '0');
         }
-
         if (rem[n] == 1) {
             return num;
         }
-
         for (int i = pos; i >= 0; i--) {
-
             while (++arr[i] <= '9') {
-
                 long tNow = rem[i] / gcd(rem[i], arr[i] - '0');
-
                 int k = 9;
-
                 for (int j = n - 1; j > i; j--) {
-
                     while (tNow % k != 0) {
                         k--;
                     }
-
                     tNow /= k;
-
                     arr[j] = (char) ('0' + k);
                 }
-
                 if (tNow == 1) {
                     return new String(arr);
                 }
             }
         }
-
         StringBuilder ans = new StringBuilder();
-
         while (t > 1) {
-
             for (int i = 9; i > 1; i--) {
-
                 while (t % i == 0) {
-
                     ans.append((char) ('0' + i));
-
                     t /= i;
                 }
             }
-
             break;
         }
-
         int ones = Math.max(n + 1 - ans.length(), 0);
-
         for (int i = 0; i < ones; i++) {
             ans.append('1');
         }
-
         ans.reverse();
-
         return ans.toString();
     }
 }
