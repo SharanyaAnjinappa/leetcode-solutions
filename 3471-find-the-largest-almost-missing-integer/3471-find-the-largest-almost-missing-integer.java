f7d0624e3b5e497ab9cdb0f2179fac695567ceeb
Sharanya.A <eng23cs0183@dsu.edu.in>
2026-08-19 10:21:24 +0530

    Time: 1 ms (99.3%), Space: 44.7 MB (75.44%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
@@ -22,7 +22,7 @@ class Solution {
             freq[i]++;
         }
         for(int i=0;i<n;i++){
-            if(k==n || (freq[nums[i]]==1 &&(k==1||i==0||i==n-1))){ans=Math.max(ans,nums[i]);}
+            if(k==n || (freq[nums[i]]==1 && (k==1||i==0||i==n-1))){ans=Math.max(ans,nums[i]);}
         }
         return ans;
     }
