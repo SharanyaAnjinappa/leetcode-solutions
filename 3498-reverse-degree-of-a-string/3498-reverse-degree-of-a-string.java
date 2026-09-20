44a72f5e9eaeb3f945096b2c2a070702c3fd251e
Sharanya.A <eng23cs0183@dsu.edu.in>
2026-09-20 21:15:08 +0530

    Time: 1 ms (100%), Space: 43.9 MB (81.16%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -3,10 +3,10 @@ class Solution {
         int sum=0;
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
-            int alphabet=ch-'a'+1;
-            int reverse=27-alphabet;
-            int pos=i+1;
-            int product=reverse*pos;
+            int alphabet=ch-'a'+1;//26-26+1=1
+            int reverse=27-alphabet;//27-1=26
+            int pos=i+1;//i=1;
+            int product=reverse*pos;//26*1;
             sum=sum+product;
         }
         return sum;
